f369b63d38eb245006d6f21253c99dc8e0fc2330
Negede <negedetekleyes@gmail.com>
2026-02-23 22:49:58 +0300

    Login page

---
+++ login.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    body {
+        background-color: lightgray;
+        font-family: verdana;
+        color: rgb(9, 20, 101);
+        margin: 0px;
+        padding: 0px;
+    }
+
+    h1 {
+        color: green;
+    }
+
+    p {
+        text-align: justify;
+    }
+
+    ul {
+        list-style-type: none;
+        display: flex;
+
+    }
+
+    li a {
+        display: block;
+        color: black;
+        padding: 20px 40px;
+        text-decoration: none;
+        float: left;
+    }
+
+    header,
+    footer {
+        background-color: aqua;
+        text-align: center;
+    }
+
+    .main_body {
+        background-color: black;
+        color: aliceblue;
+        padding: 50px 100px;
+        height: 50vh;
+        text-align: center;
+    }
+
+    button {
+        padding: 10px 20px;
+    }
+
+    .second_body {
+        margin-top: -40px;
+        margin-left: 500px;
+        display: flex;
+        width: 30%;
+        justify-content: center;
+        gap: 10px;
+
+    }
+
+    .third_body {
+        background-color: rgb(8, 58, 97);
+        padding: 70px 100px;
+        color: white;
+        border-radius: 10px;
+
+    }
+</style>
+
+<body>
+    <header>
+        <ul>
+            <h4>Email: oromiya@gmail.com || +251123456789</h4> &nbsp; &nbsp; &nbsp; &nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <li><a href="">FB</a></li>
+            <li><a href="">GO</a></li>
+        </ul>
+
+        <ul>
+            <h1>Oromiya Boarding school</h1>
+            <li><a href="">Home</a></li>
+            <li><a href="">Student</a></li>
+            <li><a href="">Teahcer</a></li>
+            <li><a href="">Course</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Login</a></li>
+        </ul>
+    </header>
+    <fieldset>
+        <h1>Login Form</h1>
+    </fieldset>
+</body>
+
+</html>
